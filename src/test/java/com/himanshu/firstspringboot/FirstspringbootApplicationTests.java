60bb62e4e3d6431480759d42018d6a6ff848807d
TheHimanshuDixit <himanshuharshdixit@gmail.com>
2025-01-31 14:32:41 +0530

    first commit

---
+++ src/test/java/com/himanshu/firstspringboot/FirstspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.himanshu.firstspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
