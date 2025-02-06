40f0fc7731729d0466f346ad245241ef2b1ea52d
TheHimanshuDixit <himanshuharshdixit@gmail.com>
2025-02-06 17:41:24 +0530

    lombok added

--- src/main/java/com/himanshu/journalApp/entity/JournalEntry.java
+++ src/main/java/com/himanshu/journalApp/entity/JournalEntry.java
@@ -6,7 +6,10 @@ import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import lombok.Data;
+
 @Document(collection = "journal")
+@Data
 public class JournalEntry {
     @Id
     private ObjectId idString;
@@ -14,45 +17,46 @@ public class JournalEntry {
     private String content;
     private Date date;
 
-    public JournalEntry() {
-    }
+    // public JournalEntry() {
+    // }
 
-    public JournalEntry(ObjectId idString, String title, String content, Date date) {
-        this.idString = idString;
-        this.title = title;
-        this.content = content;
-        this.date = date;
-    }
+    // public JournalEntry(ObjectId idString, String title, String content, Date
+    // date) {
+    // this.idString = idString;
+    // this.title = title;
+    // this.content = content;
+    // this.date = date;
+    // }
 
-    public ObjectId getIdString() {
-        return idString;
-    }
+    // public ObjectId getIdString() {
+    // return idString;
+    // }
 
-    public String getTitle() {
-        return title;
-    }
+    // public String getTitle() {
+    // return title;
+    // }
 
-    public String getContent() {
-        return content;
-    }
+    // public String getContent() {
+    // return content;
+    // }
 
-    public Date getDate() {
-        return date;
-    }
+    // public Date getDate() {
+    // return date;
+    // }
 
-    public void setIdString(ObjectId idString) {
-        this.idString = idString;
-    }
+    // public void setIdString(ObjectId idString) {
+    // this.idString = idString;
+    // }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
+    // public void setTitle(String title) {
+    // this.title = title;
+    // }
 
-    public void setContent(String content) {
-        this.content = content;
-    }
+    // public void setContent(String content) {
+    // this.content = content;
+    // }
 
-    public void setDate(Date date) {
-        this.date = date;
-    }
+    // public void setDate(Date date) {
+    // this.date = date;
+    // }
 }
