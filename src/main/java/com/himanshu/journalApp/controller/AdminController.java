1ef7c0f20030050c6a53d835c0883683f1aec2ca
TheHimanshuDixit <himanshuharshdixit@gmail.com>
2025-02-17 14:34:46 +0530

    Role based authorization

---
+++ src/main/java/com/himanshu/journalApp/controller/AdminController.java
@@ -0,0 +1,50 @@
+package com.himanshu.journalApp.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.himanshu.journalApp.entity.User;
+import com.himanshu.journalApp.service.UserService;
+
+@RestController
+@RequestMapping("/admin")
+public class AdminController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        try {
+            List<User> all = userService.getAllUsers();
+            if (all.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            } else {
+                for (User user : all) {
+                    System.out.println(user);
+                }
+                return new ResponseEntity<>(all, HttpStatus.OK);
+            }
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<User> saveAdmin(@RequestBody User user) {
+        try {
+            userService.saveAdmin(user);
+            return new ResponseEntity<>(user, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
